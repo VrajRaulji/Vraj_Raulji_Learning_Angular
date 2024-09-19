0941e47a7ab27d26844970aaa737e32d654d049f
Vrajraj13 <136127680+Vrajraj13@users.noreply.github.com>
2024-09-18 22:13:26 -0400

    Added books array with 6 instances to app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Book } from './models/book.interface';
 
 @Component({
   selector: 'app-root',
@@ -15,4 +16,14 @@ export class AppComponent {
 
   name: string = 'Vraj Raulji';
   age: number = 22;
+
+  // Initializing an array with 6 instances of book
+  books: Book[] = [
+    { title: 'The Great Gatsby', author: 'F. Scott Fitzgerald', genre: 'Classic', review: 'A great read!' },
+    { title: 'To Kill a Mockingbird', author: 'Harper Lee', genre: 'Historical Fiction', review: 'Powerful storytelling.' },
+    { title: '1984', author: 'George Orwell', genre: 'Dystopian', rating: 5, review: 'A chilling vision of the future.' },
+    { title: 'The Catcher in the Rye', author: 'J.D. Salinger', genre: 'Literary Fiction', review: 'A thought-provoking novel.' },
+    { title: 'Pride and Prejudice', author: 'Jane Austen', genre: 'Romance', rating: 4, review: 'A delightful classic.' },
+    { title: 'The Hobbit', author: 'J.R.R. Tolkien', genre: 'Fantasy', review: 'An amazing adventure.' }
+  ];
 }

68fd489b737116314edc4da13756166de0599383
Vrajraj13 <136127680+Vrajraj13@users.noreply.github.com>
2024-10-09 22:43:48 -0400

    Injected BookService and used it to retrieve books

--- src/app/book-list/book-list.component.ts
+++ src/app/book-list/book-list.component.ts
@@ -1,18 +1,21 @@
-import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common'; // Import CommonModule
-import { BookListItemComponent } from '../book-list-item/book-list-item.component'; // Import BookListItemComponent
-import { MOCK_CONTENT } from '../data/mock-content';
-
-
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { BookListItemComponent } from '../book-list-item/book-list-item.component';
+import { BookService } from '../book.service'; // Import the BookService
 
 @Component({
   selector: 'app-book-list',
   standalone: true,
-  imports: [CommonModule, BookListItemComponent], // Include CommonModule and BookListItemComponent
+  imports: [CommonModule, BookListItemComponent],
   templateUrl: './book-list.component.html',
   styleUrls: ['./book-list.component.scss']
 })
-export class BookListComponent {
-  books = MOCK_CONTENT;
+export class BookListComponent implements OnInit {
+  books: any[] = [];
+  constructor(private bookService: BookService) {}
+  ngOnInit(): void {
+    this.bookService.getBooks().subscribe((data) => {
+      this.books = data;
+    });
+  }
 }
-
